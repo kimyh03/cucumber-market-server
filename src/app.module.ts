450d51ca19e257c2ace76babf9bc70b4fccdbc88
Hoony <kimyh03@gmail.com>
2020-09-03 20:02:33 +0900

    Connect to TypeORM(Mysql)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'cucumbermarket',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
