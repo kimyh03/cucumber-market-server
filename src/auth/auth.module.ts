fd00eea6e6c59f6c2da19340b3b582d87c104cb5
Hoony <kimyh03@gmail.com>
2020-09-14 12:49:00 +0900

    BugFix ConfigModule(.env)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule,
     JwtModule.register({
-      secret: 'process.env.JWT_SECERET',
+      secret: process.env.JWT_SECERET,
     }),
     UserModule,
   ],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,14 @@ import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: 'process.env.JWT_SECERET',
+      secret: process.env.JWT_SECERET,
     }),
   ],
   providers: [UserService, UserResolver, AuthService],
